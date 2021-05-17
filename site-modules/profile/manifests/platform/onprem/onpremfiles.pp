3db9962e80f5f44ad26ac0a576c1bb75a00503ef
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-05-17 15:40:51 -0700

    changed syntax

--- site-modules/profile/manifests/platform/onprem/onpremfiles.pp
+++ site-modules/profile/manifests/platform/onprem/onpremfiles.pp
@@ -1,6 +1,6 @@
 class profile::platform::onprem::onpremfiles {
    file { '/var/www/html/graphic.png':
      ensure => file,
-     source => "/etc/puppetlabs/code/environments/production/site-modules/profile/manifests/platform/onprem.png",
+     source => "puppet:///profile/onprem.png",
    }
 }
