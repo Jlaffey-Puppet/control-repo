2854211db05ca706d94d957eadab2e5d8023c179
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-05-17 14:09:07 -0700

    Initial creation

---
+++ site-modules/profile/manifests/platform/baseline/linux/packages.pp
@@ -0,0 +1,19 @@
+class profile::platform::baseline::linux::packages {
+
+  $pkgs = ['unzip','wget']
+
+  ensure_packages($pkgs, {ensure => installed})
+
+  if $::osfamily == 'RedHat' {
+    include ::epel
+  }
+
+  unless getvar('trusted.external.servicenow.u_enforced_packages').empty {
+    $packages = parsejson($trusted['external']['servicenow']['u_enforced_packages'])
+    $packages.each |$package,$ensure|{
+      package { $package:
+        ensure => $ensure
+      }
+    }
+  }
+}
