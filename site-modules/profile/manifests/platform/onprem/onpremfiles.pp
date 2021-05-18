030e79e296a916b3a5c04f0258896bbce1d00214
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-05-18 14:38:28 -0700

    updated file

--- site-modules/profile/manifests/platform/onprem/onpremfiles.pp
+++ site-modules/profile/manifests/platform/onprem/onpremfiles.pp
@@ -1,6 +1,6 @@
 class profile::platform::onprem::onpremfiles {
    file { '/var/www/html/graphic.png':
      ensure => file,
-     source => "puppet:///modules/profile/onprem.png",
+     source => "puppet:///modules/profile/newonprem.png",
    }
 }
